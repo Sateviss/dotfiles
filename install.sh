22666dc60e237b4cc821d4b3b76ab4a93c348598
Eugene Pisarchick <sateviss@gmail.com>
2019-05-23 18:31:47 +0300

    INITIAL COMMIT

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+if [[ "$*" == *-n* ]]
+then
+    export CONFIG_DIR=$(pwd) >> ~/.profile
+fi
+
+mkdir -p ~/.config/i3 && ln -fs $(pwd)/i3.conf ~/.config/i3/config
+mkdir -p ~/.config/i3blocks && ln -fs $(pwd)/i3blocks.conf ~/.config/i3blocks/config
+mkdir -p ~/.config/rofi && ln -fs $(pwd)/rofi.conf ~/.config/rofi/config
