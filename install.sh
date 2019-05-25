7e583b100e8030ab38ed4fcb80ec54732c937b0b
Eugene Pisarchick <sateviss@gmail.com>
2019-05-25 16:15:24 +0300

    update

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ fi
 mkdir -p ~/.config/i3 && ln -fs $(pwd)/i3.conf ~/.config/i3/config
 mkdir -p ~/.config/i3blocks && ln -fs $(pwd)/i3blocks.conf ~/.config/i3blocks/config
 mkdir -p ~/.config/rofi && ln -fs $(pwd)/rofi.conf ~/.config/rofi/config
+mkdir -p ~/.config/polybar && ln -fs $(pwd)/polybar.conf ~/.config/polybar/config
