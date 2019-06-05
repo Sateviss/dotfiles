be15d6fe0b299233309f7c2f3c083ae44b7e42bd
Eugene Pisarchick <sateviss@gmail.com>
2019-06-05 20:38:21 +0300

    up

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ mkdir -p ~/.config/i3blocks && ln -fs $SCRIPTPATH/i3blocks.conf ~/.config/i3bloc
 mkdir -p ~/.config/rofi && ln -fs $SCRIPTPATH/rofi.conf ~/.config/rofi/config
 mkdir -p ~/.config/polybar && ln -fs $SCRIPTPATH/polybar.conf ~/.config/polybar/config
 mkdir -p ~/.config/conky && ln -fs $SCRIPTPATH/conky.conf ~/.config/conky/conky.conf
+mkdir -p ~/.config/compton && ln -fs $SCRIPTPATH/compton.conf ~/.config/compton/compton.conf
