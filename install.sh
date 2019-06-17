b79eb175d68da1327d0972dd39fb9cc82a14e636
Eugene Pisarchick <sateviss@gmail.com>
2019-06-17 11:33:27 +0300

    stowed

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #! /bin/sh
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+# SCRIPT=$(readlink -f "$0")
+# SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [[ "$*" == *-n* ]]
-then
-    export CONFIG_DIR=$SCRIPTPATH >> ~/.profile
-fi
+# if [[ "$*" == *-n* ]]
+# then]
+#     export CONFIG_DIR=$SCRIPTPATH >> ~/.profile
+# fi
 
-mkdir -p ~/.config/i3 && ln -fs $SCRIPTPATH/i3.conf ~/.config/i3/config
-mkdir -p ~/.config/i3blocks && ln -fs $SCRIPTPATH/i3blocks.conf ~/.config/i3blocks/config
-mkdir -p ~/.config/rofi && ln -fs $SCRIPTPATH/rofi.conf ~/.config/rofi/config
-mkdir -p ~/.config/polybar && ln -fs $SCRIPTPATH/polybar.conf ~/.config/polybar/config
-mkdir -p ~/.config/conky && ln -fs $SCRIPTPATH/conky.conf ~/.config/conky/conky.conf
-mkdir -p ~/.config/compton && ln -fs $SCRIPTPATH/compton.conf ~/.config/compton/compton.conf
+echo "Stowing modules from module_list..."
+
+for module in $(cat module_list); do
+    stow $module && echo "Stowed $(tput bold)$module$(tput sgr0)"
+done
+
+echo "Modules stowed!"
+
+# grep -qxF 'export PATH=$PATH:~/.bin' ~/.profile || { echo -e '\nexport PATH=$PATH:~/.bin' >> ~/.profile; echo "Added .bin to PATH"; }
