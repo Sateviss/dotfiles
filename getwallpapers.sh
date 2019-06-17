d1ce3afae92097b1bd5e00127afe293f7c50b567
Eugene Pisarchick <sateviss@gmail.com>
2019-06-17 19:07:52 +0300

    wallpaper shenanigans

--- getwallpapers.sh
+++ getwallpapers.sh
@@ -11,5 +11,5 @@ while read p; do
     name=$(echo $p | awk '{print $1}')
     url=$(echo $p | awk '{print $2}')
     echo "Downloading $(tput bold)$name$(tput sgr0) from $(tput bold)$url$(tput sgr0)"
-    curl -# $url -o ~/Pictures/Wallpapers/$name
+    curl -L -# $url -o ~/Pictures/Wallpapers/$name
 done < wallpaper_list
